e89043949638758caeef835a5bbafac3b5abd2c9
Marco Alexi Sta Ana <msta001@ucr.edu>
2019-11-09 12:28:06 -0800

    added barebones to it and img!

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('public'))
+
 app.get('/', function(req, res){
     res.sendFile(`${__dirname}/public/index.html`)
 })
 
 app.listen(PORT, function(){
-    console.log('server started @ port 8080');
+    console.log('Server started!!');
 })
